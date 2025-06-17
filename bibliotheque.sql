89664d1a323c4f106738d45447af750e71656b66
Madow-s <Madeleinesarr60@gmail.com>
2025-06-17 12:05:17 +0200

    Create bibliotheque.sql

---
+++ bibliotheque.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mar. 17 juin 2025 à 12:03
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `bibliotheque`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `emprunt`
+--
+-- Erreur de lecture de structure pour la table bibliotheque.emprunt : #1932 - Table 'bibliotheque.emprunt' doesn't exist in engine
+-- Erreur de lecture des données pour la table bibliotheque.emprunt : #1064 - Erreur de syntaxe près de 'FROM `bibliotheque`.`emprunt`' à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `librairie`
+--
+-- Erreur de lecture de structure pour la table bibliotheque.librairie : #1932 - Table 'bibliotheque.librairie' doesn't exist in engine
+-- Erreur de lecture des données pour la table bibliotheque.librairie : #1064 - Erreur de syntaxe près de 'FROM `bibliotheque`.`librairie`' à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `livre`
+--
+-- Erreur de lecture de structure pour la table bibliotheque.livre : #1932 - Table 'bibliotheque.livre' doesn't exist in engine
+-- Erreur de lecture des données pour la table bibliotheque.livre : #1064 - Erreur de syntaxe près de 'FROM `bibliotheque`.`livre`' à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `maison_edition`
+--
+-- Erreur de lecture de structure pour la table bibliotheque.maison_edition : #1932 - Table 'bibliotheque.maison_edition' doesn't exist in engine
+-- Erreur de lecture des données pour la table bibliotheque.maison_edition : #1064 - Erreur de syntaxe près de 'FROM `bibliotheque`.`maison_edition`' à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+-- Erreur de lecture de structure pour la table bibliotheque.utilisateur : #1932 - Table 'bibliotheque.utilisateur' doesn't exist in engine
+-- Erreur de lecture des données pour la table bibliotheque.utilisateur : #1064 - Erreur de syntaxe près de 'FROM `bibliotheque`.`utilisateur`' à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur_role`
+--
+-- Erreur de lecture de structure pour la table bibliotheque.utilisateur_role : #1932 - Table 'bibliotheque.utilisateur_role' doesn't exist in engine
+-- Erreur de lecture des données pour la table bibliotheque.utilisateur_role : #1064 - Erreur de syntaxe près de 'FROM `bibliotheque`.`utilisateur_role`' à la ligne 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
